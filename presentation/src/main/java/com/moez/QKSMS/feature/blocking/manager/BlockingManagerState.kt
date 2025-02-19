0b84c296f641b475838617b96021e0997c15af1a
Inhishonor <inhishonor@protonmail.com>
2025-02-18 17:55:58 -0800

    Update BlockingManagerState.kt

--- presentation/src/main/java/com/moez/QKSMS/feature/blocking/manager/BlockingManagerState.kt
+++ presentation/src/main/java/com/moez/QKSMS/feature/blocking/manager/BlockingManagerState.kt
@@ -2,6 +2,7 @@ package dev.octoshrimpy.quik.feature.blocking.manager
 
 data class BlockingManagerState(
     val blockingManager: Int = 0,
+    val spamBlockerInstalled: Boolean = false,
     val callBlockerInstalled: Boolean = false,
     val callControlInstalled: Boolean = false,
     val siaInstalled: Boolean = false
